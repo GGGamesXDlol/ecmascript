fd09f4562bf02369bcddcbb0f8b254ade87da4b8
GGGamesXDlol <63313393+GGGamesXDlol@users.noreply.github.com>
2022-06-28 14:00:17 +0200

    XD

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+let numbers = [1,2,3,7,8];
+if(numbers.includes(7))
+{
+    console.log("7")
+}
+else
+{
+    console.log("7n't")
+}
+
+let base = 4;
+let exponent = 3;
+let result = base ** exponent;
+
+console.log(result);
