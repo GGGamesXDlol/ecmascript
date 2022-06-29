aef57448c2495e96cb9e433c020964641bbe227b
GGGamesXDlol <63313393+GGGamesXDlol@users.noreply.github.com>
2022-06-29 13:53:10 +0200

    argsdf

--- src/es8/index.js
+++ src/es8/index.js
@@ -7,3 +7,51 @@ const data =
 
 const entries = Object.entries(data);
 console.log(entries);
+console.log(entries.length);
+
+const data2 =
+{
+    frontend: "Pedro",
+    backend: "Fredo",
+    design: "Huevo",
+}
+
+const values = Object.values(data2);
+console.log(values);
+console.log(values.length);
+
+const string = "hello";
+console.log(string.padStart(8, "hi "));
+console.log(string.padEnd(12, " -----"));
+console.log("food".padEnd(12, "  -----"));
+
+const helloWorld = () => 
+{
+    return new Promise((resolve, reject) => 
+    {
+        (true)
+            ? setTimeout(() => resolve("helloWorld"), 3000)
+            : reject(new Error("Test error"))
+    }) 
+}
+
+const helloAsync = async () =>
+{
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () =>
+{
+    try 
+    {
+        const hello = await helloAsync();
+        console.log(hello);
+    } 
+    catch (error) 
+    {
+        console.log(error);
+    }
+}
